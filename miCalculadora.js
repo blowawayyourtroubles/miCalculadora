ce78f5e524f4f9e623fe5b8543133d01ead61694
German Gibbs <gibbsgerman@gmail.com>
2025-11-10 12:01:38 -0400

    first commit

---
+++ miCalculadora.js
@@ -0,0 +1,9 @@
+const buttonLayout = [
+    ['CE', 'C', '/', '*'],
+    ['7', '8', '9', '-'],
+    ['4', '5', '6', '+'],
+    ['1', '2', '3', '='],
+    ['00', '0',]
+];
+
+const DisplayCalculator = document.createElement('display')
