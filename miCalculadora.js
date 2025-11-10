4fed7e812f1febc6075e1e4f1c48f81d2073f651
German Gibbs <84513918+blowawayyourtroubles@users.noreply.github.com>
2025-11-10 14:52:59 -0400

    nueva calculadora arreglada estilos css y javascript simple

--- miCalculadora.js
+++ miCalculadora.js
@@ -1,11 +1,29 @@
-// const buttonLayout = [
-//     ['CE', 'C', '/', '*'],
-//     ['7', '8', '9', '-'],
-//     ['4', '5', '6', '+'],
-//     ['1', '2', '3', '='],
-//     ['0',]
-// ];
+const calcTitle = document.getElementById("calcTitle");
+calcTitle.append("Mi Calculadora");
 
-// const DisplayCalculator = document.createElement('display')
+const display = document.getElementById("display");
 
-w
+function appendToDisplay(input) {
+    display.value += input;
+};
+
+function clearDisplay() {
+    display.value = ""
+};
+
+
+function deleteLast() {
+    if (display.value.lenght > 1) {
+        display.value = display.value.slice(0, -1);
+    }
+    else {
+        display.value = "0";
+    }
+};
+function caclculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value("Error")
+    }
+};
